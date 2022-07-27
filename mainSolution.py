e755b737f0b9152180688402b5a54d5bac2536d8
edalenelee <S10244812@connect.np.edu.sg>
2022-07-27 16:07:31 +0800

    web api

--- mainSolution.py
+++ mainSolution.py
@@ -3,9 +3,4 @@ api_key= "RNPKUJP5Z7T08SJ0"
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
 response = requests.get(url)
-<<<<<<< HEAD
-print(response.json())
-print(response)
-=======
 data = response.json()
->>>>>>> 77f3eb71f3975e943ef787242b2cfb182cb18ef3
