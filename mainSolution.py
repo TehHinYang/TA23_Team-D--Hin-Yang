27683fe0e6e99e9e90239a25a6be270f9420b2dd
TehHinYang <s10243065@connect.np.edu.sg>
2022-07-29 22:46:49 +0800

    updates

--- mainSolution.py
+++ mainSolution.py
@@ -1,7 +1,4 @@
-import requests
-api_key= "RNPKUJP5Z7T08SJ0"
-url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
-
-response = requests.get(url)
-data = response.json()
-print(data)
+from pathlib import Path
+import cash_on_hand
+fp = Path.cwd()/"summary_report.txt"
+fp.touch()
