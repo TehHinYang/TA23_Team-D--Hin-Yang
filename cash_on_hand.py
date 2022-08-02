584126439c9b14038ab16414acf58b59caa68cca
yuting0812 <s10239848@connect.np.edu.sg>
2022-08-02 15:21:46 +0800

    hi

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -13,5 +13,22 @@ def cash_on_hand_function():
       data = line[1]
       amt_coh.append(data)
       day.append(days)
+    num = 1
+    difference =[]
+    messages = []
+    while num < len(amt_coh):
+      diff = float(amt_coh[num]) - float(amt_coh[num-1])
+      difference.append(diff)
+      difference.sort()
+      num += 1
+      if diff <= 0:
+        diff = api.forex*abs(diff)
+        messages.append(f"[CASH DEFICIT] DAY: {day[num-1]}, AMOUNT: SGD{round(diff,2)}")
+      else:
+        continue
+    if difference[0] > 0:
+      messages.append("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    return messages
       
 print(cash_on_hand_function())
+
