1a0aea361c552a35cc71063257f5c5d9e866dbde
TehHinYang <s10243065@connect.np.edu.sg>
2022-08-03 00:33:08 +0800

    main soultions

--- mainSolution.py
+++ mainSolution.py
@@ -4,3 +4,19 @@ import cash_on_hand,overheads,profit_loss,api
 def mainsoultion_function():
     fp = Path.cwd()/"summary_report.txt"
     fp.touch()
+    data = cash_on_hand.cash_on_hand_function()
+    data1 = profit_loss.profit_loss_function()
+    num = 0
+    num1 = 0
+    with fp.open(mode="w", encoding="UTF-8", newline="") as file:
+        file.writelines(api.api_funtion())
+    with fp.open(mode="a", encoding="UTF-8", newline="") as file:
+        file.writelines(f"\n{overheads.overheads_function()}")
+        while num < len(data):
+            file.writelines(f"\n{data[num]}")
+            num += 1
+        while num1 < len(data1):
+            file.writelines(f"\n{data1[num1]}")
+            num1 += 1
+
+mainsoultion_function()
