bbc5bbcfaa6050b4d8bcc71666b90934d1a44eea
yuting0812 <s10239848@connect.np.edu.sg>
2022-08-02 12:33:07 +0800

    commit

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -3,9 +3,3 @@ import csv
 fp = Path.cwd()/"summary_report.txt"
 fp.touch()
 
-file_path = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
-with file_path.open(mode="r", encoding="UTF-8") as file:
-    reader = csv.reader(file)
-    next(reader)
-    for line in reader:
-        data = line[1]
